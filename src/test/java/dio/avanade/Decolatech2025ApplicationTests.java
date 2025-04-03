74ff0fe0dc5f11fe10b5e0ed32e9bf733f17c2c7
Lilian <lilian.siq@hotmail.com>
2025-04-02 21:47:08 -0300

    Initial commit

---
+++ src/test/java/dio/avanade/Decolatech2025ApplicationTests.java
@@ -0,0 +1,13 @@
+package dio.avanade;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Decolatech2025ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
